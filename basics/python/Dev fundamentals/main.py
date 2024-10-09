66f64bf89839882f1edcb19a59c3d3e01e2fb05f
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-10-09 11:55:44 +0530

    add: sample project(with pandas and matplotlib)

---
+++ basics/python/Dev fundamentals/main.py
@@ -0,0 +1,11 @@
+#operator precedence
+print(20 - 3 * 4)
+print((20 - 3) ** 2)
+
+#order
+# ()
+#power **
+# * / 
+# + -
+print(bin(5))
+print(int(0b101))
--- basics/python/main.py
+++ basics/python/main.py
@@ -1,3 +1,9 @@
-print(5+2)
-print("Hellooooow")
-print(type(2+4))
+# print(5+2)
+# print("Hellooooow")
+# print(type(2+4))
+
+#math functions
+print(round(3.1))
+print(round(3.9))
+print(abs(-2))
+
